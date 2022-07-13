195e3945a21c9f56f9cd28c2da714bf7f87abf2d
dandanmania <danielkimus@gmail.com>
2022-07-12 18:34:47 -0700

    fix getAccessToken typo

--- src/api.js
+++ src/api.js
@@ -79,14 +79,15 @@ export const getAccessToken = async () => {
     if (!accessToken || tokenCheck.error) {
         await localStorage.removeItem('access_token');
         const searchParams = new URLSearchParams(window.location.search);
-    }
-    const code = await searchParams.get('code');
-    if (!code) {
-        const results = await axios.get(
-            'https://nqe9xiax80.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url'
-        );
-        const { authUrl } = results.data;
-        return (window.location.href = authUrl);
+        const code = await searchParams.get('code');
+        if (!code) {
+            const results = await axios.get(
+                'https://nqe9xiax80.execute-api.us-west-1.amazonaws.com/dev/api/get-auth-url'
+            );
+            const { authUrl } = results.data;
+            return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
     }
     return accessToken;
 }
