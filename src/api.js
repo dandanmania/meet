e7f2373dde684d1f6083b13df0d5eefcdf8e7430
dandanmania <danielkimus@gmail.com>
2022-07-18 15:48:37 -0700

    Update api.js for checkToken typo

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ export const extractLocations = (events) => {
 
 const checkToken = async (accessToken) => {
     const result = await fetch(
-        `https://www.googleapis.com/oath2/v1/tokeninfo?access_token=${accessToken}`
+        `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
     )
     .then((res) => res.json())
     .catch((error) => error.json());
